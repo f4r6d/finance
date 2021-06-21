7fc34dea76dc3fbcc447cb8774bc95491077aa3b
Farshid Gholizadeh <66841257+f4r6d@users.noreply.github.com>
2021-06-21 22:20:27 +0430

    Update app.py

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ uri = uri.replace("postgres://", "postgresql://", 1)
 db = SQL(uri)
 
 
-# db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 10000.00)")
+db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 10000.00)")
 
-# db.execute("CREATE TABLE trans (user_id INTEGER, symbol TEXT NOT NULL, shares NUMERIC NOT NULL, price NUMERIC NOT NULL DEFAULT 0, ts  TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id))")
+db.execute("CREATE TABLE trans (user_id SERIAL , symbol TEXT NOT NULL, shares NUMERIC NOT NULL, price NUMERIC NOT NULL DEFAULT 0, ts  TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id))")
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
